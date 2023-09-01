b24626a3627cea899632d7540c3210e7baaad832
ddeshha <ddeshh4@gmail.com>
2023-09-01 07:51:59 +0300

    new files

---
+++ 12.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//  Make a new variable called (Full_string) that concatenate string_one and string_two
+
+
+                    $string_one = "Eraa";
+
+                    $string_two = "Soft";
+
+                    $Full_string = $string_one . $string_two;
+
---
+++ 14.php
@@ -0,0 +1,11 @@
+<?php
+/*
+
+14-Write a PHP script to split the following string.
+Sample string: 'ErraSoft'
+Expected Output: Er/ra/So/ft
+
+*/
+
+
+
